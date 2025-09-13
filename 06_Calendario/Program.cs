01010c3f6de93beb7e33710dc5a0def6375c3086
luis.gssoeiro <luisgustavomarilia52@gmail.com>
2025-09-12 21:41:41 -0300

    nova classe do calendário em outra janela

---
+++ 06_Calendario/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _06_Calendario;
+
+namespace _09_Calendario
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+
+            //Console.Write("Digite o Mês (1 a 12): ");
+            //int mes = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o Ano (ex: 2023)");
+            int ano = int.Parse(Console.ReadLine());
+
+            Calendario calendario;
+            for (int mes = 1; mes <= 12; mes++)
+            {
+                calendario = new Calendario(ano, mes);
+                calendario.ImprimirCalendario();
+            }
+
+            Console.WriteLine("\n\n");
+
+
+       }
+
+    }
+}
