b064cd0da70b931fa97714afdb9591900f2da989
luis.gssoeiro <luisgustavomarilia52@gmail.com>
2025-09-15 22:28:28 -0300

    Tarefa do professor , ainda não Concluída.

---
+++ 08_Aluno/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08_Aluno
+{
+    internal class Aluno
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+            int idade;
+            double nota1, nota2;
+            double media;
+            string situacao;
+
+            Console.WriteLine("Digite seu nome:");
+           nome = Console.ReadLine();
+          
+            Console.WriteLine("Digite sua idade: ");
+            idade = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("Digite sua primeira Nota: ");
+            nota1 = int.Parse(Console.ReadLine()); 
+
+            Console.WriteLine("Digite sua Segunda Nota: ");
+            nota2 = int.Parse(Console.ReadLine());
+        }
+    }
+}
