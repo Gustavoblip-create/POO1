08d44ec054cc5765c517eb7294eb3893541a92cc
luis.gssoeiro <luisgustavomarilia52@gmail.com>
2025-09-18 22:20:26 -0300

    Herança

---
+++ 09_Heranca/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09_Heranca
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Não é possivel instanciar uma classe abstrata
+            //Pessoa obj = new Pessoa();
+
+            PessoaFisica pessoaFisica = new PessoaFisica();
+            pessoaFisica.Nome = "Geraldo";
+            pessoaFisica.CPF = "999.999.999-99";
+            pessoaFisica.Telefone = "(14) 99999-9999";
+            pessoaFisica.Imprimir();
+
+            PessoaJuridica pessoaJuridica = new PessoaJuridica();
+            pessoaJuridica.Nome = "senac Marilia";
+            pessoaJuridica.CNPJ = "999.999.999-99";
+            pessoaJuridica.Telefone = "(014) 9999-9999";
+            pessoaJuridica.Imprimir();
+
+
+        }
+    }
+}
