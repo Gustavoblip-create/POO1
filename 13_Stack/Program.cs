b9b25bfde718b58b0c0627dbf8a1d5ae9a8fd779
luis.gssoeiro <luisgustavomarilia52@gmail.com>
2025-09-26 19:52:08 -0300

    Stack - Coleçoes

---
+++ 13_Stack/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13_Stack
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> Numeros = new Stack<int>();
+             
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Digite 5 numeros");
+                int numero = int.Parse(Console.ReadLine());
+                Numeros.Push(numero);
+            }
+            Console.WriteLine("\nPilha de numeros  Decrescente");
+            foreach (int n in Numeros)
+            {
+                Console.WriteLine(n);
+            }
+
+           
+        }
+    }
+}
