0a856a5f1610fd054b453436603e16960a2d5046
luis.gssoeiro <luisgustavomarilia52@gmail.com>
2025-09-29 20:25:01 -0300

    16_Enum - Coleçoes

---
+++ 15_InumerationColecoes/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+// Criando o enum
+enum Nivel
+{
+    Facil = 1,
+    Medio = 2,
+    Dificil = 3
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("=== Escolha o nível de dificuldade ===");
+        Console.WriteLine("1 - Fácil");
+        Console.WriteLine("2 - Médio");
+        Console.WriteLine("3 - Difícil");
+        Console.Write("Digite o número da opção: ");
+
+        // Lê a escolha do usuário
+        int escolha = int.Parse(Console.ReadLine());
+
+        // Converte o número para o enum
+        Nivel nivelEscolhido = (Nivel)escolha;
+
+        // Mostra o que o usuário escolheu
+        Console.WriteLine($"\nVocê escolheu o nível: {nivelEscolhido}");
+
+        // Você pode usar em condições também
+        if (nivelEscolhido == Nivel.Facil)
+        {
+            Console.WriteLine("Boa sorte! Começando no modo fácil.");
+        }
+        else if (nivelEscolhido == Nivel.Medio)
+        {
+            Console.WriteLine("Modo médio ativado. Prepare-se!");
+        }
+        else if (nivelEscolhido == Nivel.Dificil)
+        {
+            Console.WriteLine("Modo difícil! Boa sorte, guerreiro!");
+        }
+        else
+        {
+            Console.WriteLine("Opção inválida.");
+        }
+
+        Console.ReadLine(); // Mantém o console aberto
+    }
+}
---
+++ 16_Enum/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16_Enum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Sem Ultilização de Enum");
+           // List<int> diaDasemanaSemenum = new List<int> { 0,1,2,3,4,5,6};
+            List<int> diaDasemanaSemenum = new List<int> { 1,2,3,4,5,6,7};
+           foreach (var item in diaDasemanaSemenum)
+            {
+                //if(item == 0) Console.WriteLine("Domingo");
+                //else if (item == 1) Console.WriteLine("Segunda");
+                //else if (item == 2) Console.WriteLine("Terça");
+                //else if (item == 3) Console.WriteLine("Quarta");
+                //else if (item == 4) Console.WriteLine("Quinta");
+                //else if (item == 5) Console.WriteLine("Sexta");
+                //else if (item == 6) Console.WriteLine("Sabado");
+                
+                if(item == 1) Console.WriteLine("Domingo - Numero: 1");
+                else if (item == 2) Console.WriteLine("Segunda - Numero: 2");
+                else if (item == 3) Console.WriteLine("Terça - Numero: 3");
+                else if (item == 4) Console.WriteLine("Quarta - Numero: 4");
+                else if (item == 5) Console.WriteLine("Quinta - Numero: 5");
+                else if (item == 6) Console.WriteLine("Sexta - Numero: 6");
+                else if (item == 7) Console.WriteLine("Sabado - Numero: 7");
+            }
+
+            Console.WriteLine("\n\nCom a ultilização do Enum");
+            List <DiadaSemana> diaDaSemanaComEnum = new List<DiadaSemana>()
+            {
+                DiadaSemana.Domingo,
+                 DiadaSemana.Segunda,
+                DiadaSemana.Terca,
+                DiadaSemana.Quarta,
+                DiadaSemana.Quinta,
+                DiadaSemana.Sexta,
+                DiadaSemana.Sabado
+            };
+
+            foreach (var item in diaDaSemanaComEnum)
+            {
+                Console.WriteLine($"{item} Numero: {(int)item}");
+            }
+        }
+    }
+    public enum DiadaSemana
+    {
+            Domingo = 1,
+            Segunda ,
+            Terca ,
+            Quarta,
+            Quinta,
+            Sexta,
+            Sabado
+    }
+
+}
