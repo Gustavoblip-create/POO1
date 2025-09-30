a0bc08fe9e83a797fb7d19b7054b255c7270759c
luis.gssoeiro <luisgustavomarilia52@gmail.com>
2025-09-29 21:01:58 -0300

    CalendarioFeriado com as Colecoes

---
+++ 17_Calendario/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+
+namespace _17_Calendario
+{
+    internal class progam
+    {
+        static void Main(string[] args)
+
+        {
+
+            //Console.Write("Digite o Mês (1 a 12): ");
+           // int mes = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o Ano (ex: 2023)");
+            int ano = int.Parse(Console.ReadLine());
+
+            Calendario calendario;
+            for (int mes = 1; mes <= 12; mes++)
+            {
+                calendario = new Calendario(ano, mes);
+                calendario.ImprimirCalendario();
+            
+
+            Console.WriteLine("\n\n");
+
+            }
+            Console.ReadKey();
+       }
+
+    }
+}
