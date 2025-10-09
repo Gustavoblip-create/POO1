1b2eb35fae7fa909f92ade9f942bbcce75f153f3
luis.gssoeiro <luisgustavomarilia52@gmail.com>
2025-10-09 19:53:56 -0300

    Tratameto de exceção usando  o tipo FormatException

---
+++ 21_ConversaoNumeroComExcecao/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21_ConversaoNumeroComExcecao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool NumeroValido = false;
+            int num;
+
+            while(!NumeroValido)
+
+
+            try
+            {
+
+                Console.WriteLine("Digite um Numero Inteiro: ");
+                num = int.Parse(Console.ReadLine());
+
+                Console.WriteLine($"Parabéns, o Dobro do seu numero é:{num + num} ");
+                NumeroValido = true;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("ERRO! Você digitou um numero invalido, vamos tentar de novo!");
+               
+            }
+
+
+        }
+    }
+}
