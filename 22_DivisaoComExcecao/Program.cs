7d33d9c75a891f00f30d056e1e1c9810ecd85d2c
luis.gssoeiro <luisgustavomarilia52@gmail.com>
2025-10-09 20:09:34 -0300

    _22_DivisaoComExcecao, Ultilizando try , catch e também o Finally

---
+++ 22_DivisaoComExcecao/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22_DivisaoComExcecao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, num2;
+           // bool dadosValidos = false;
+
+            //while (!dadosValidos)
+
+                try
+                {
+
+                    Console.WriteLine("Digite um numero Inteiro: ");
+                    num = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Dividido por outro numero inteiro : ");
+                    num2 = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine($" A divisão dos dois numeros é: {num / num2}");
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Por favor digite numeros inteiros");
+                }
+                catch (DivideByZeroException)
+                {
+                    Console.WriteLine("Não Divida por zero, Engraçadinho");
+                }
+                finally
+                {
+                    Console.WriteLine("\nOperação finalizada. Obrigado por utilizar o programa!");
+                }
+        }
+    }
+}
